3b1cc25c88aff803a45666a3877e5cd95216275a
achieva.gemilang <achievafuturagemilang@gmail.com>
2025-03-12 23:37:59 +0700

    chore: add logo to webapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,49 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import Navbar from "@/components/layout/navbar"
-import { ThemeProvider } from "@/components/theme-provider"
-import Footer from "@/components/layout/footer"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/layout/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Achieva Gemilang",
-  description: "Professional portfolio showcasing projects, experiences, and blog posts",
-}
+  description:
+    "Professional portfolio showcasing projects, experiences, and blog posts",
+  icons: {
+    icon: [
+      {
+        url: "/AG_logo.webp",
+        href: "/AG_logo.webp",
+      },
+    ],
+    apple: {
+      url: "/AG_logo.webp",
+      href: "/AG_logo.webp",
+    },
+    shortcut: {
+      url: "/AG_logo.webp",
+      href: "/AG_logo.webp",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="flex flex-col min-h-screen">
             <Navbar />
             <main className="flex-grow">{children}</main>
@@ -30,6 +52,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
