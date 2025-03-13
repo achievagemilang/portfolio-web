d57ad2a86f95d9c8dccf8b681bfe70ba98c93cbd
achieva.gemilang <achievafuturagemilang@gmail.com>
2025-03-13 16:52:47 +0700

    chore: adjust dependencies, config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,31 @@
+import createMDX from '@next/mdx';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    mdxRs: true,
+  },
   images: {
-    domains: ['placeholder.com'],
+    domains: ['placeholder.com', 'images.unsplash.com', 'cdn-images-1.medium.com'],
   },
   // This is important for Contentlayer to work with Next.js
-  webpack: (config) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      // https://github.com/vercel/next.js/issues/7755
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          child_process: false,
+          fs: false,
+          path: false,
+          'builtin-modules': false,
+          worker_threads: false,
+        },
+      };
+    }
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
@@ -14,5 +34,10 @@ const nextConfig = {
   },
 };
 
-// Remove the contentlayer wrapper until we fully set it up
-export default nextConfig;
+const withMDX = createMDX();
+
+// Export the combined config
+export default withMDX({
+  ...nextConfig,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+});
